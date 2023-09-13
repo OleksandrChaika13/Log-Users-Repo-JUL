bcde6e0fc8c4855d4227142d0106e37146fc4f66
OleksandrChaika13 <axelrod1347@gmail.com>
2023-09-13 18:47:50 +0300

    Added README and SQLs files

---
+++ INFO/SQLs.sql
@@ -0,0 +1,32 @@
+
+-- Створення БД
+CREATE DATABASE users_db;
+
+-- Запит на створення таблиці
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  phone TEXT NOT NULL,
+  email TEXT NOT NULL
+)
+
+-- Створення юзеру
+INSERT INTO users (name, phone, email) VALUES(?, ?, ?)
+
+-- Вибір юзеру з таблиці
+SELECT * FROM users
+
+-- Оновлення всіх даних юзера
+UPDATE users SET name = ?, phone = ?, email = ? WHERE id = ?
+
+-- Оновлення лише імені
+UPDATE users SET name = ? WHERE id = ?
+
+-- Оновлення лише телефону
+UPDATE users SET phone = ? WHERE id = ?
+
+-- Оновлення лише email
+UPDATE users SET email = ? WHERE id = ?
+
+-- Видалення юзеру
+DELETE FROM users WHERE id = ?
